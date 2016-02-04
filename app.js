ac14c6d4db6e9ab40812b776687152751b7c7ac9
Rahul Rout <rrout@BANL138af53e2.local>
2016-02-04 08:30:30 +0530

    Seed

--- app.js
+++
@@ -1,9 +0,0 @@
-angular.module('forms', [
-  'ngRoute',
-  'forms.landing',
-])
-
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-  .otherwise({redirectTo: '/landing'})
-}])
