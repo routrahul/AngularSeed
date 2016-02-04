6451fbfd0b2959b58414c1e6a80376a8080ee658
Rahul Rout <rrout@BANL138af53e2.local>
2016-02-04 21:50:42 +0530

    Changes with Directives

--- app/components/version/version-directive.js
+++ app/components/version/version-directive.js
@@ -6,4 +6,71 @@ angular.module('myApp.version.version-directive', [])
   return function(scope, elm, attrs) {
     elm.text(version);
   };
-}]);
+}])
+
+.directive('currentTime',['$interval','dateFilter',function($interval,dateFilter){
+  function link(scope,element,attrs){
+    var format,timeoutId;
+    function updateTime(){
+      element.text(dateFilter(new Date(),format));
+    }
+
+    scope.$watch(attrs.currentTime,function(value){
+      format = value;
+      updateTime();
+    })
+
+    element.on('$destroy',function(){
+      $interval.cancel(timeoutId);
+    })
+
+    timeoutId = $interval(function(){
+      updateTime()
+    },1000);
+  }
+
+  return {
+    link:link
+  };
+}])
+
+.directive('myTabs',function(){
+  return{
+    restrict:'E',
+    transclude:true,
+    scope:{},
+    controller:['$scope',function($scope){
+      var panes = $scope.panes = [];
+      $scope.select = function(pane){
+        angular.forEach(panes,function(pane){
+          pane.selected = false;
+        });
+        pane.selected = true;
+      };
+
+      this.addPane = function(pane){
+        if(panes.length === 0)
+        {
+          $scope.select(pane);
+        }
+        panes.push(pane);
+      };
+    }],
+    templateUrl:'/app/components/version/my-tabs.html'
+  }
+})
+
+.directive('myPane',function(){
+  return {
+    require: '^^myTabs',
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    link: function(scope, element, attrs, tabsCtrl) {
+      tabsCtrl.addPane(scope);
+    },
+    templateUrl: '/app/components/version/my-pane.html'
+  };
+})
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,6 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope',function($scope) {
+  $scope.format = 'MM/dd/yy h:mm:ss a';
 }]);
