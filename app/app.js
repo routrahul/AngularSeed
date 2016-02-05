9c731ada0d3036b2215a861aa134c1f3d68ebf99
Rahul Rout <rrout@BANL138af53e2.local>
2016-02-05 09:32:22 +0530

    recent changes

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.view3',
+  'myApp.view4',
+  'myApp.view5',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
